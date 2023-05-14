27bc1bea7f80e3b4e3fd2d708953e640a64eac4f
YediHub <133596747+YediHub@users.noreply.github.com>
2023-05-14 19:25:35 +0100

    Add files via upload

---
+++ text.py
@@ -0,0 +1,94 @@
+import pygame
+from vector import Vector2
+from constants import *
+
+class Text(object):
+    def __init__(self, text, color, x, y, size, time=None, id=None, visible=True):
+        self.id = id
+        self.text = text
+        self.color = color
+        self.size = size
+        self.visible = visible
+        self.position = Vector2(x, y)
+        self.timer = 0
+        self.lifespan = time
+        self.label = None
+        self.destroy = False
+        self.setupFont("PressStart2P-Regular.ttf")
+        self.createLabel()
+
+    def setupFont(self, fontpath):
+        self.font = pygame.font.Font(fontpath, self.size)
+
+    def createLabel(self):
+        self.label = self.font.render(self.text, 1, self.color)
+
+    def setText(self, newtext):
+        self.text = str(newtext)
+        self.createLabel()
+
+    def update(self, dt):
+        if self.lifespan is not None:
+            self.timer += dt
+            if self.timer >= self.lifespan:
+                self.timer = 0
+                self.lifespan = None
+                self.destroy = True
+
+    def render(self, screen):
+        if self.visible:
+            x, y = self.position.asTuple()
+            screen.blit(self.label, (x, y))
+
+
+class TextGroup(object):
+    def __init__(self):
+        self.nextid = 10
+        self.alltext = {}
+        self.setupText()
+        self.showText(READYTXT)
+
+    def addText(self, text, color, x, y, size, time=None, id=None):
+        self.nextid += 1
+        self.alltext[self.nextid] = Text(text, color, x, y, size, time=time, id=id)
+        return self.nextid
+
+    def removeText(self, id):
+        self.alltext.pop(id)
+        
+    def setupText(self):
+        size = TILEHEIGHT
+        self.alltext[SCORETXT] = Text("0".zfill(8), WHITE, 0, TILEHEIGHT, size)
+        self.alltext[READYTXT] = Text("CUIDADIN CON LOS FANTASMAS", YELLOW, 1.15*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
+        self.alltext[PAUSETXT] = Text("¿PAUSA PARA CAFE?", YELLOW, 6*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
+        self.alltext[GAMEOVERTXT] = Text("LA CAGASTE, Y LO SABES", YELLOW, 2.5*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
+        self.alltext[YOUWINTXT] = Text("¡ASI QUE NO ERAS TAN MALO!", YELLOW, 1.15*TILEWIDTH, 20*TILEHEIGHT, size, visible=False)
+        self.addText("PUNTACOS", WHITE, 0, 0, size)
+        self.addText("EL COMILON", WHITE, 18*TILEWIDTH, 10, size)
+
+    def update(self, dt):
+        for tkey in list(self.alltext.keys()):
+            self.alltext[tkey].update(dt)
+            if self.alltext[tkey].destroy:
+                self.removeText(tkey)
+
+    def showText(self, id):
+        self.hideText()
+        self.alltext[id].visible = True
+
+    def hideText(self):
+        self.alltext[READYTXT].visible = False
+        self.alltext[PAUSETXT].visible = False
+        self.alltext[GAMEOVERTXT].visible = False
+        self.alltext[YOUWINTXT].visible = False
+
+    def updateScore(self, score):
+        self.updateText(SCORETXT, str(score).zfill(8))
+
+    def updateText(self, id, value):
+        if id in self.alltext.keys():
+            self.alltext[id].setText(value)
+
+    def render(self, screen):
+        for tkey in list(self.alltext.keys()):
+            self.alltext[tkey].render(screen)
